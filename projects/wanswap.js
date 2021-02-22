6121b41fe202a8390435dfe30afcd9710565ebbb
olafsnow45 <olafsnow0405@gmail.com>
2021-02-22 12:17:37 +0000

    feat: new adapters

--- projects/wanswap.js
+++ projects/wanswap.js
@@ -5,8 +5,8 @@ const utils = require('./helper/utils');
 *
 *****************/
 async function fetch() {
-  let staked = await utils.fetchURL('https://api.bt.finance/api/stats')
-  return staked.data.data.totalLockedValue;
+  var totalTvl = await utils.fetchURL('http://wanstakeinsight.com/wanswap_api/get_tvl')
+  return totalTvl.data.result;
 }
 
 module.exports = {
