8dc0b1b5296df386b8ceb26268e4a71fe3325c24
olafsnow45 <olafsnow0405@gmail.com>
2021-03-24 10:09:30 +1100

    Updating new staking / lending assets

--- projects/alpha.js
+++ projects/alpha.js
@@ -4,10 +4,9 @@ const utils = require('./helper/utils');
 * ==> Correct adapter needs to be created.
 *
 *****************/
-
 async function fetch() {
-  let response = await utils.fetchURL('https://api.pancakeswap.finance/api/v1/stat')
-  return response.data.total_value_locked_all;
+  var staked = await utils.fetchURL('https://api-homora.alphafinance.io/pools')
+  return staked.data.tvlUSD;
 }
 
 module.exports = {
