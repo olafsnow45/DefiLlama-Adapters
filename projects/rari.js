d5fe39e415ecdd57869b6e0e396e115265d4bfbc
olafsnow45 <olafsnow0405@gmail.com>
2021-04-05 14:49:27 +0200

    fix powerindex

--- projects/rari.js
+++ projects/rari.js
@@ -1,7 +1,7 @@
-const fetch = require("node-fetch");
+const fetchUrl = require("node-fetch");
 
 async function fetch() {
-  const { tvl } = fetch("https://app.rari.capital/api/stats").then((res) =>
+  const { tvl } = await fetchUrl("https://app.rari.capital/api/stats").then((res) =>
     res.json()
   );
 
