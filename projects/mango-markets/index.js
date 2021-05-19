fed732aa956412c2ef145e5b5a54a2228653b567
olafsnow45 <olafsnow0405@gmail.com>
2021-05-19 19:49:20 +0200

    simplify

--- projects/mango-markets/index.js
+++ projects/mango-markets/index.js
@@ -12,6 +12,12 @@ function findClosestToDate(values, date) {
             min = val
         }
     }
+    if(Math.abs(new Date(min.time).getTime()-date) > 24*3600*1000){
+        return {
+            totalDeposits: 0,
+            totalBorrows: 0
+        }
+    }
     return min
 }
 
