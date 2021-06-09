30cef4906b734409107db29f768d98240ad27ba6
olafsnow45 <olafsnow0405@gmail.com>
2021-06-10 01:39:00 +0800

    tvl calc for InsurAce

--- projects/raydium.js
+++ projects/raydium.js
@@ -1,18 +1,12 @@
-const retry = require("./helper/retry");
+const retry = require('./helper/retry')
 const axios = require("axios");
 
 async function fetch() {
-  const response = (
-    await retry(async (bail) => await axios.get("https://api.raydium.io/pairs"))
-  ).data;
+  var response = await retry(async bail => await axios.get('https://api.raydium.io/tvl'))
 
-  const liqArrPerPool = response.map((pool) => pool.liquidity);
-
-  const tvl = liqArrPerPool.reduce((a, b) => a + b, 0);
-
-  return tvl || 0;
+  return response.data || 0;
 }
 
 module.exports = {
-  fetch,
-};
+  fetch
+}
