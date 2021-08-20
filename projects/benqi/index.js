ef4bccb9a8cb6a2772de563d94259b20894f4384
olafsnow45 <olafsnow0405@gmail.com>
2021-08-21 00:01:26 +0100

    AsgardDAO Finance TVL Adapter and Updating of OlympusDAO Finance TVL Adapter

--- projects/benqi/index.js
+++ projects/benqi/index.js
@@ -1,10 +1,11 @@
 const {getCompoundV2Tvl} = require('../helper/compound')
-const {transformAvaxAddress, fixAvaxBalances} = require('../helper/portedTokens')
+const {transformAvaxAddress} = require('../helper/portedTokens')
 
 async function tvl(...params) {
     const transformAdress= await transformAvaxAddress()
     const balances = await getCompoundV2Tvl("0x486af39519b4dc9a7fccd318217352830e8ad9b4", "avax", transformAdress)(...params)
-    fixAvaxBalances(balances)
+    balances['avalanche-2'] = Number(balances['0x9dEbca6eA3af87Bf422Cea9ac955618ceb56EfB4'])/1e18
+    delete balances['0x9dEbca6eA3af87Bf422Cea9ac955618ceb56EfB4']
     return balances
 }
 
